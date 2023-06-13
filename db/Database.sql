1b7d532d03dfcbc762ee816a6cff081173a3b52f
EdisonP <133362311+edions@users.noreply.github.com>
2023-06-13 11:29:46 +0800

    Update

--- db/Database.sql
+++ db/Database.sql
@@ -1,60 +1,60 @@
 -- Table to store product information
 CREATE TABLE [dbo].[Product]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Name] VARCHAR(50) NULL, 
-    [Price] INT NULL, 
-    [Stock] INT NULL, 
-    [Unit] INT NULL, 
-    [Category] VARCHAR(50) NULL
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [NAME] VARCHAR(50) NULL, 
+    [PRICE] INT NULL, 
+    [STOCK] INT NULL, 
+    [UNIT] INT NULL, 
+    [CATEGORY] VARCHAR(50) NULL
 );
 
 -- Table to store category information
 CREATE TABLE [dbo].[Category]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [CategoryItem] VARCHAR(50) NULL,
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [CATEGORYITEM] VARCHAR(50) NULL,
 );
 
 -- Table to track the history of stock updates
 CREATE TABLE [dbo].[History]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [ProductID] INT NULL, 
-    [Added Stocks] INT NULL, 
-    [Date] DATETIME NULL,
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [PRODUCTID] INT NULL, 
+    [ADDED STOCKS] INT NULL, 
+    [DATE] DATETIME NULL,
 );
 
 -- Table to store items in the shopping cart
 CREATE TABLE [dbo].[Cart]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Name] VARCHAR(50) NULL, 
-    [Price] INT NULL, 
-    [Quantity] INT NULL,
-    [ProductId] INT NULL
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [NAME] VARCHAR(50) NULL, 
+    [PRICE] INT NULL, 
+    [QUANTITY] INT NULL,
+    [PRODUCTID] INT NULL
 );
 
 -- Table to store transaction information
 CREATE TABLE [dbo].[Transaction]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Date] DATETIME NULL, 
-    [Subtotal] INT NULL, 
-    [Cash] INT NULL, 
-    [Percent] VARCHAR(50) NULL, 
-    [Amount] DECIMAL(18, 2) NULL, 
-    [Total] INT NULL, 
-    [Change] DECIMAL(18, 2) NULL, 
-    [Uid] VARCHAR(MAX) NULL,
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [DATE] DATETIME NULL, 
+    [SUBTOTAL] INT NULL, 
+    [CASH] INT NULL, 
+    [PERCENT] VARCHAR(50) NULL, 
+    [AMOUNT] DECIMAL(18, 2) NULL, 
+    [TOTAL] INT NULL, 
+    [CHANGE] DECIMAL(18, 2) NULL, 
+    [UID] VARCHAR(MAX) NULL,
 );
 
 -- Table to store ordered items in a transaction
 CREATE TABLE [dbo].[Orders]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Uid] VARCHAR(MAX) NULL, 
-    [Name] VARCHAR(50) NULL, 
-    [Price] INT NULL, 
-    [Quantity] INT NULL
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [UID] VARCHAR(MAX) NULL, 
+    [NAME] VARCHAR(50) NULL, 
+    [PRICE] INT NULL, 
+    [QUANTITY] INT NULL
 );
